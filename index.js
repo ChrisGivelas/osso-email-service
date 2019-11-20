e081053695fa8766c6f72ac82e8281ac73bb6dc0
Christopher Givelas <chris_g_50@hotmail.com>
2019-11-20 12:45:42 -0500

    Express server

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 "use strict";
-const nodemailer = require("nodemailer");
 const credentials = require("./credentials");
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport(credentials.transport);
+const express = require("express");
 
-    await transporter.sendMail(
+
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport(credentials.transport);
+
+const app = express();
+
+app.use(express.json());
+
+const emailRouter = express.Router();
+
+emailRouter.post('/', async function(req, res) {
+    console.log("Email")
+    console.log(req.body);
+    const {name, email, subject, message} = req.body;
+
+    const emailRes = await transporter.sendMail(
         credentials.getMailObject({
-            name: "Fred Foo",
-            email: "foo@example.com",
-            subject: "Hello",
-            message: "Hello world?"
+            name,
+            email,
+            subject,
+            message,
         })
     );
-}
 
-main().catch(console.error);
+    console.log(emailRes);
+
+    res.send(emailRes);
+});
+
+app.use("/email", emailRouter);
+
+const PORT = process.env.PORT || 3210;
+
+console.log("Starting server on port " + PORT + "...");
+app.listen(PORT);
